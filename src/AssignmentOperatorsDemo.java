e001aea10e5279ff53715c48edb6c05bfbe0b736
Blanco <katherinhe.blanco@accenture.com>
2024-11-08 10:52:46 -0500

    updated files

---
+++ src/AssignmentOperatorsDemo.java
@@ -0,0 +1,20 @@
+
+public class AssignmentOperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int j, k;
+j = 10; //j gets the value of 10
+j = 5; // j gets the value of 5. The previous value is overwritten.
+k = j; // k gets the value of 5.
+System.out.println("j is : " + j);
+System.out.println("k is : " + k);
+
+//              Multiple Assignments
+k = j = 10; // (k = (j = 10))
+System.out.println("j is : " + j);
+System.out.println("k is : " + k);
+
+	}
+
+}
